2f99d15849362a6a0ca56d9da85bbdc216e23e76
jsavory-94 <james.pa.savory@gmail.com>
2018-03-05 16:35:48 +0100

    corrected app.js file

--- app.js
+++ app.js
@@ -1,72 +1,27 @@
-require("dotenv").config();
-const cors = require('cors');
 const express = require('express');
 const path = require('path');
-const logger = require('morgan');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const index = require('./routes/index');
-const userStats = require('./routes/user-stats');
-const mongoose = require('mongoose');
-const auth = require('./routes/auth');
-const session    = require('express-session');
-const MongoStore = require('connect-mongo')(session);
 
-mongoose.connect(process.env.MONGODB_URI);
+const pkg = require('./package.json');
+const app = express();
 
-var app = express();
+const forceSSL = function () {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      const path = 'https://' + req.get('Host') + req.url;
+      return res.redirect(path);
+    }
+    next();
+  };
+};
 
-  app.use(cors({
-    credentials: true,
-    origin: ['http://localhost:4200']
-  }));
+app.use(forceSSL());
 
-app.use(session({
-  store: new MongoStore({
-    mongooseConnection: mongoose.connection,
-    ttl: 24 * 60 * 60 // 1 day
-  }),
-  secret: 'some-string',
-  resave: true,
-  saveUninitialized: true,
-  cookie: {
-    maxAge: 24 * 60 * 60 * 1000
-  }
-}));
+app.use(express.static(path.join(__dirname, '/dist')));
 
-app.use(function (req, res, next) {
-  app.locals.user = req.session.currentUser;
-  next();
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(path.join(__dirname, '/dist/index.html')));
 });
 
-// view engine setup
+const port = process.env.PORT || 8080;
 
-// uncomment after placing your favicon in /public
-  
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-
-
-app.use('/', index);
-app.use('/auth', auth);
-app.use('/user-stats', userStats);
-
-
-// catch 404 and forward to error handler
-app.use((req, res, next) => {
-  res.status(404).json({error: 'not found'});
-});
-
-app.use((err, req, res, next) => {
-  // always log the error
-  console.error('ERROR', req.method, req.path, err);
-
-  // only render if the error ocurred before sending the response
-  if (!res.headersSent) {
-    res.status(500).json({error: 'unexpected'});
-  }
-});
-
-module.exports = app;
+app.listen(port, () => console.log(pkg.name + ' ' + pkg.version + ' listening on ' + port));
